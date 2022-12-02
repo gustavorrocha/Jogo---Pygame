f34138900246ccfc0d8a4cf51b0d9721f4ab7a8e
gustavorrocha <gustavoreisrocha.grr@gmail.com>
2022-12-02 18:59:57 -0300

    Adicione o lenhador no jogo

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Snake:
         self.__old_coords = [] # Lista com as coordenadas das partes do corpo da cobra
         self.__direction = "" # Armazena a direção da cobra
         self.__direction_cache = "" # Caso a direção não possa ser mudada, armazena um cache até que seja possível
-        self.count_frames = 0 
+        self.count_frames = 0 # Cria o contador de frames
         self.draw() # Desenha a cobra
     
     def draw(self):
@@ -50,9 +50,13 @@ class Snake:
         self.direction = self.__direction_cache # Tenta alterar a direção até que seja viável
 
     def move(self):
-        self.count_frames += 1
+        self.count_frames += 1 # Incrementa o contador de frames
+
+        # Remove as coordenadas antigas que não serã usadas
         while len(self.__old_coords) > self.body_size:
             self.__old_coords.pop(-1)
+
+        # Utiliza o contador de frames para alterar a velocidade da cobra        
         if self.count_frames % round(1/self.speed) == 0:
             self.check_direction("") # Tenta mudar a direção, caso ela não tenha sido mudada
 
@@ -68,7 +72,7 @@ class Snake:
                 self.y -= 20
             elif self.direction == "down":
                 self.y += 20    
-
+                
         self.draw() # Redesenha a cobra na sua nova posição
     
     def eat_apple(self):
@@ -87,6 +91,71 @@ class Apple:
     def draw(self):
         pygame.draw.rect(self.screen, (255,0,0), (self.x, self.y, self.size, self.size)) # Desenha a maçã
 
+class Lumberjack:
+    def __init__(self, screen, size):
+        self.screen = screen
+        resolution = self.screen.get_size() # Armazena a resolução da tela
+        self.size = size
+        self.x = randint(0,resolution[0] - size)//size*size # Randomiza a posição x do lenhador
+        self.y = randint(0,resolution[1] - size)//size*size # Randomiza a posição y do lenhador
+        self.direction = 0 # Armazena a direção do lenhador
+        self.count_frames = 0 # Cria o contador de frames
+        self.is_smart = randint(0,1) # Randomiza se o lenhador será inteligente (irá atrás das maçãs) ou não
+        self.draw() # Desenha a maçã
+
+    def draw(self):
+        pygame.draw.rect(self.screen, (0,0,255), (self.x, self.y, self.size, self.size)) # Desenha a maçã
+    
+    def stay_in_screen(self):
+        resolution = self.screen.get_size() # Armazena a resolução da tela
+
+        # Não permite que o lenhador ultrapasse os limites da tela
+        if self.x == 0 and self.direction == 0:
+            self.direction = 1
+        elif self.x == resolution[0] - self.size and self.direction == 1:
+            self.direction = 0
+        if self.y == 0 and self.direction == 2:
+            self.direction = 3
+        elif self.y == resolution[1] - self.size and self.direction == 3:
+            self.direction = 2
+
+    def follow_target(self, target):
+        # Persegue um objeto alvo
+        if target.x < self.x:
+            self.direction = 0
+        elif target.x > self.x:
+            self.direction = 1
+        elif target.y < self.y:
+            self.direction = 2
+        elif target.y > self.y:
+            self.direction = 3
+
+    def move(self, apple):
+        self.count_frames += 1 # Incremeta o contador de frames
+
+        # Utiliza o contador de frames para fazer com que a movimentação do lenhador deja mais lenta      
+        if self.count_frames % 10 == 0:
+
+            if self.is_smart: # Se o lenhador for inteligente, o faz seguir a maçã
+                self.follow_target(apple)
+
+            # 1% de chance de alterar a direção do lenhador
+            if randint(1,10) == 1:
+                self.direction = randint(0,3)
+    
+            self.stay_in_screen() # Faz com que o lenhador não saia da tela
+
+            # Movimentação básica do lenhador
+            if self.direction == 0:
+                self.x -= 20
+            elif self.direction == 1:
+                self.x += 20
+            elif self.direction == 2:
+                self.y -= 20
+            elif self.direction == 3:
+                self.y += 20
+
+        self.draw() # Redesenha o lenhador na sua nova posição
 
 class Game:
     def __init__(self, resolution):
@@ -95,6 +164,7 @@ class Game:
         self.screen.fill((0,0,0)) # Preenche o background com a cor preta
         self.snake = Snake(self.screen) # Cria o objeto COBRA
         self.apple = Apple(self.screen, 20) # Cria o objeto MAÇÃ
+        self.lumberjack = Lumberjack(self.screen, 20) # Cria o objeto LENHADOR
         self.font = pygame.font.SysFont("comicsans", 30, True) # Define a fonte dos textos
         self.score = 0 # Cria um atributo de pontuação
 
@@ -108,6 +178,7 @@ class Game:
             resolution = self.screen.get_size() # Armazena a resolução da tela 
             self.screen.blit(self.font.render("Pontuação: " + str(self.score), 1, (255,255,255)), (resolution[0] - 250,10)) # Escreve a pontuação na tela
             self.apple.draw() # Desenha a maça
+            self.lumberjack.move(self.apple) # Roda a movimentação do lehador
             self.snake.move() # Roda a função para a cobra se mover
 
             for event in pygame.event.get():
@@ -123,6 +194,18 @@ class Game:
                 self.score += 1 # Adiciona 1 à pontuação
                 self.snake.body_size += 1 # Adiciona 1 ao corpo da cobra
                 self.apple = Apple(self.screen, 20) # Cria uma nova maça na tela
+
+            # Condicional do lenhador passar pela maçã
+            if (self.apple.x,self.apple.y) == (self.lumberjack.x, self.lumberjack.y):
+                del self.apple # Remove a maça
+                self.score -= 1 # Remove 1 da pontuação
+                self.apple = Apple(self.screen, 20) # Cria uma nova maça na tela
+
+            # Condicional da cobra passar pelo lenhador
+            if (self.snake.x,self.snake.y) == (self.lumberjack.x, self.lumberjack.y):
+                del self.lumberjack # Remove o lenhador
+                self.score += 1 # Adiciona 1 à pontuação
+                self.lumberjack = Lumberjack(self.screen, 20) # Cria um novo lenhador na tela
 
             pygame.display.update() # Atualiza a tela
 
